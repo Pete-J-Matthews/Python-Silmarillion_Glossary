6017c4d48c463e9baeda71a4f11d1c10802ebd52
Pete Matthews <102467456+Pete-J-Matthews@users.noreply.github.com>
2022-07-05 22:27:21 +0100

    Add files via upload

---
+++ sil_lib.py
@@ -0,0 +1,9 @@
+sil_definitions = [
+['Adanedhel', 'Name given to Túrin in Nargothrond'], 
+['Adunakhôr', 'Lord of the West, name taken by the nineteenth King of Númenor, the first to do so in the Adûnaic (Númenórean) tongue; his name in Quenya was Herunúmen.'], 
+['Adurant', 'The sixth and most southerly of the tributaries of Gelion in Ossiriand. The name means double stream, referring to its divided course about the island of Tol Galen.'],
+['Aeglos', 'Snow-point, the spear of Gil-galad'], 
+['Aegnor', 'The fourth son of Finarfin, who with his brother Angrod held the northern slopes of Dorthonion; slain in the Dagor Bragollach. The name means Fell Fire'], 
+['Aelin-uial', 'Meres of Twilight, where Aros flowed into Sirion'], 
+['Aerandir', 'Sea-wanderer, one of the three mariners who accompanied Eärendil on his voyages'], 
+['Aerin', 'A kinswoman of Húrin in Dor-lómin; taken as wife by Brodda the Easterling; aided Morwen after the Nirnaeth Arnoediad.'], 'good luck']]
